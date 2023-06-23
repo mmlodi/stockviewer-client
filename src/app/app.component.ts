0ef0293e02ca2f2cb68617df0ecf14931e46166f
Mateus Lodi <mmischel@hotmail.com>
2023-06-22 21:20:11 -0300

    items iniciais do projeto front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'StockViewer';
+
+  constructor(private http : HttpClient){
+
+  }
 }
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {}
+
+  constructor( private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    // this.accountService.login(this.model).subscribe({
+    //   next: _ =>  this.router.navigateByUrl('/members'),
+    // })
+  }
+
+  logout(){
+    // this.accountService.logout();
+    // this.router.navigateByUrl('/');
+  }
+}
